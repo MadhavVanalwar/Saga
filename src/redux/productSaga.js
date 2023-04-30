892f621fe526bfd15f1742e8f61642208afe3582
Madhav Vanalwar <madhavvanalwar143@gmail.com>
2023-04-30 11:04:47 +0530

    commit

--- src/redux/productSaga.js
+++ src/redux/productSaga.js
@@ -2,17 +2,17 @@ import { takeEvery, put } from 'redux-saga/effects'
 import { PRODUCT_LIST, SEARCH_PRODUCT, SET_PRODUCT_LIST } from './constant';
 
 function* getProducts() {
-    let data = yield fetch('http://localhost:3500/products');
-    data = yield data.json();
-    console.warn("action is called", data)
-    yield put({type: SET_PRODUCT_LIST, data})
+	let data = yield fetch("http://localhost:4000/products")
+	data = yield data.json()
+	console.warn("action is called", data)
+	yield put({ type: SET_PRODUCT_LIST, data })
 }
 
 function* searchProducts(data) {
-    let result = yield fetch(`http://localhost:3500/products?q=${data.query}`);
-    result = yield result.json();
-    console.warn("action is called", result)
-    yield put({type: SET_PRODUCT_LIST, data:result})
+	let result = yield fetch(`http://localhost:4000/products?q=${data.query}`)
+	result = yield result.json()
+	console.warn("action is called", result)
+	yield put({ type: SET_PRODUCT_LIST, data: result })
 }
 
 function* productSaga() {
